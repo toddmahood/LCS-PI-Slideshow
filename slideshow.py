210e92087a5ac273105a051291488984ad6ed39f
Todd Mahood <todd.mahood.20@cnu.edu>
2024-11-13 14:12:46 -0500

    update video rendering

--- slideshow.py
+++ slideshow.py
@@ -1,10 +1,11 @@
 import os
-import cv2
 import json
 import pygame
 import asyncio
-from collections import deque
+import logging
 from PIL import Image
+from collections import deque
+from pyvidplayer2 import Video
 from pillow_heif import register_heif_opener, register_avif_opener
 
 # Register HEIF and AVIF format support
@@ -21,8 +22,11 @@ local_media_directory = data["local_media_directory"]
 announcement_directory = data["announcement_directory"]
 slide_duration = data["slide_duration"]
 announcement_duration = data["announcement_duration"]
+log_path = data["log_directory"]
 supported_image_formats = ('.ras', '.im', '.grib', '.qoi', '.jfif', '.pxr', '.pbm', '.rgb', '.pnm', '.hdf', '.sgi', '.jpf', '.icns', '.blp', '.jpeg', '.bw', '.rgba', '.ppm', '.tiff', '.pgm', '.xpm', '.mpeg', '.ftu', '.bufr', '.gif', '.ftc', '.pcx', '.jpg', '.fits', '.pcd', '.ps', '.apng', '.pfm', '.emf', '.jpe', '.tif', '.vst', '.fli', '.cur', '.dib', '.jp2', '.gbr', '.tga', '.icb', '.j2c', '.webp', '.ico', '.png', '.jpc', '.vda', '.h5', '.flc', '.msp', '.eps', '.j2k', '.iim', '.wmf', '.bmp', '.dcx', '.jpx', '.psd', '.fit', '.xbm', '.mpg', '.dds', '.heic', '.avif', '.exif', '.xmp', '.iptc', '.svg', '.lbm')
-supported_video_formats = ('.avi', '.mp4', '.mov', '.mkv') # Probably more, but no official opencv list due to differences in codecs per install?
+supported_video_formats = ('.avi', '.mp4', '.mov') # Probably more, but no official opencv list due to differences in codecs per install?
+
+logging.basicConfig(filename=f'{log_path}', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.DEBUG)
 
 # Set up the display
 pygame.init()
@@ -30,7 +34,6 @@ pygame.display.set_caption("LCS Slideshow")
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) 
 screen_size = screen.get_size()
 clock = pygame.time.Clock()
-
 media_queue = deque(maxlen=5)
 
 class event_handler:
@@ -61,7 +64,7 @@ async def load_image(image_path, screen_size):
         img = Image.open(image_path)
         img_width, img_height = img.size
         if (img_width < 1200) or (img_height < 720):
-            print(f"Omitting image, too small...")
+            logging.info(f"Omitting image: {image_path}, too small...")
             return None
 
         # Utilize EXIF data to properly orient image
@@ -76,8 +79,7 @@ async def load_image(image_path, screen_size):
                 elif orientation_value == 8:
                     img = img.rotate(90, expand=True)
         except Exception as e:
-            print(f"Error obtaining EXIF information for {image_path}: {e}\nUsing default orientation...")
-            pass
+            logging.debug(f"Error obtaining EXIF information for {image_path}: {e}\nUsing default orientation...")
 
         # This nifty function handles the scaling for us!
         img.thumbnail(screen_size, Image.LANCZOS)
@@ -87,7 +89,7 @@ async def load_image(image_path, screen_size):
         # Convert Pillow image to pygame surface
         img_surface = pygame.image.fromstring(img.tobytes(), img.size, 'RGB').convert()
     except Exception as e:
-        print(f"Error loading image with PIL {image_path}: {e}")
+        logging.error(f"Error loading image with PIL {image_path}: {e}")
         try:
             img = pygame.image.load(image_path)
             img_width, img_height = img.get_size()
@@ -110,16 +112,17 @@ async def load_image(image_path, screen_size):
             # Scale the image
             img_surface = pygame.transform.scale(img, (new_img_width, new_img_height)).convert()
         except Exception as e:
-            print(f"Error loading image with Pygame {image_path}: {e}")
+            logging.error(f"Error loading image with Pygame {image_path}: {e}")
             return None
     return img_surface
 
 async def load_video(video_path):
-    try:
-        cap = cv2.VideoCapture(video_path)
-        return cap
+    try: 
+        vid = Video(video_path)
+        vid.change_resolution(screen_size[1])
+        return vid
     except Exception as e:
-        print(f"Error loading video with opencv {video_path}: {e}")
+        logging.error(f"Error loading video with pyvidplayer2 {video_path}: {e}")
         return None
     
 async def prepare_media_queue(screen_size):
@@ -139,7 +142,7 @@ async def prepare_media_queue(screen_size):
             queue_full = (len(media_queue) >= 5)
             while queue_full:
                 await asyncio.sleep(0.1)
-                print("Queue full, waiting for media to be displayed.")
+                logging.info("Queue full, waiting for media to be displayed.")
                 queue_full = (len(media_queue) >= 5)
 
             media_type = None
@@ -158,16 +161,16 @@ async def prepare_media_queue(screen_size):
                 else:
                     media_type = "Video"
             else:
-                print(f"Unsupported media file: {media_file}")
+                logging.error(f"Unsupported media file: {media_file}")
                 if num_announcements != 0:
                     media_type = "Announcement"
                     num_announcements -= 1
-            if media:
+            if media != None:
                 if media_type == "Announcement":
                     media_queue.append((media, True))
                 else:
                     media_queue.append((media, False))
-                print(f"{media_type} added to queue.")
+                logging.info(f"{media_type} added to queue.")
 
 async def display_image(image, screen, fade_duration=1000, slide_duration=slide_duration):
     image_rect = image.get_rect(center=screen.get_rect().center)
@@ -201,69 +204,74 @@ async def display_image(image, screen, fade_duration=1000, slide_duration=slide_
         pygame.time.delay(fade_duration // 51)
         # await asyncio.sleep(fade_duration // 5100) 
 
-async def scale_video_frame(frame, screen_size):
-    frame_height, frame_width = frame.shape[:2]
-    screen_width, screen_height = screen_size
-
-    # Calculate the aspect ratios
-    frame_aspect_ratio = frame_width / frame_height
-    screen_aspect_ratio = screen_width / screen_height
-
-    # Determine the scaling factor
-    if frame_aspect_ratio > screen_aspect_ratio:
-        # Video is wider than the screen, scale based on width
-        new_width = screen_width
-        new_height = int(screen_width / frame_aspect_ratio)
-    else:
-        # Video is taller or fits well, scale based on height
-        new_height = screen_height
-        new_width = int(screen_height * frame_aspect_ratio)
-
-    # Resize the frame with the calculated dimensions
-    scaled_frame = cv2.resize(frame, (new_width, new_height))
-    return scaled_frame
-
-
 async def display_video(video, screen):
-    cap = video
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # Convert OpenCV frame to pygame surface
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = await scale_video_frame(frame, screen_size) # Scale to screen size
-        surface = pygame.surfarray.make_surface(frame.swapaxes(0, 1))
-        screen.blit(surface, surface.get_rect(center=screen.get_rect().center))
+    video_width, video_height = video.current_size
+    x_pos = (screen_size[0] - video_width) // 2
+    y_pos = (screen_size[1] - video_height) // 2
+    video_frame_rate = video.frame_rate
+    logging.debug(f"Video Frame Rate: {video_frame_rate}")
+    # Create overlay to mimic fading in and out.
+    overlay = pygame.Surface(screen_size)
+    overlay.fill((0, 0, 0))
+
+    total_video_duration = video.duration  
+    fade_in_duration = 1
+    fade_out_duration = 1
+
+    if total_video_duration < (fade_in_duration + fade_out_duration):
+        scale_factor = total_video_duration / (fade_in_duration + fade_out_duration)
+        fade_in_duration *= scale_factor
+        fade_out_duration *= scale_factor
+
+    while video.active:
+        current_video_time = video.get_pos()
+        if current_video_time < fade_in_duration:  # Fade-in
+            fade_alpha = int(255 - (current_video_time / fade_in_duration) * 255)  # Start at 255, decrease to 0
+        elif current_video_time >= (total_video_duration - fade_out_duration):
+            fade_alpha = int(((current_video_time - (total_video_duration - fade_out_duration)) / fade_out_duration) * 255)  # Start at 0, increase to 255
+        else:
+            fade_alpha = 0
+        logging.debug(f"Fade Alpha: {fade_alpha}")
+
+        if video.draw(screen, (x_pos,y_pos), force_draw=False):
+            if fade_alpha > 0:
+                overlay.set_alpha(fade_alpha)
+                screen.blit(overlay, (0, 0))
+            pygame.display.update()
         events_to_handle = list(pygame.event.get())
         await handle_events(events_to_handle)
-        pygame.display.update()
-        clock.tick(60)  # Frame rate for video
-    cap.release()
+        clock.tick(video_frame_rate)  # Frame rate for video
+    video.close()
 
 async def pygame_loop(framerate_limit=60):
-    task = asyncio.create_task(prepare_media_queue(screen_size))
-    while True:
-        if len(media_queue) > 0:
-            media, isAnnouncement = media_queue.popleft()
-            if isinstance(media, pygame.Surface):
-                if isAnnouncement:
-                    await display_image(media, screen, slide_duration=announcement_duration)
+    try:
+        task = asyncio.create_task(prepare_media_queue(screen_size))
+        while True:
+            try:
+                if len(media_queue) > 0:
+                    media, isAnnouncement = media_queue.popleft()
+                    if isinstance(media, pygame.Surface):
+                        if isAnnouncement:
+                            await display_image(media, screen, slide_duration=announcement_duration)
+                        else:
+                            await display_image(media, screen)
+                    elif isinstance(media, Video): 
+                        await display_video(media, screen)
+                    else:
+                        logging.error(f"Unrecognized object. {type(media)}")
+                    events_to_handle = list(pygame.event.get())
+                    await handle_events(events_to_handle)
                 else:
-                    await display_image(media, screen)
-            elif isinstance(media, cv2.VideoCapture): 
-                await display_video(media, screen)
-            else:
-                print(f"Unrecognized object. {type(media)}")
-            events_to_handle = list(pygame.event.get())
-            await handle_events(events_to_handle)
-        else:
-            await asyncio.sleep(0.1)
+                    await asyncio.sleep(0.1)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit() 
-                exit() # Exit on quit event
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit() 
+                        exit() # Exit on quit event
+            except Exception as e:
+                logging.error(f"Error displaying image or processing events: {e}")
+    except Exception as e:
+        logging.error(f"Error in pygame_loop: {e}")
 
 if __name__ == "__main__":
     asyncio.run(pygame_loop(60))
